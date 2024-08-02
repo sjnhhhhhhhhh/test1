d36c39de1fe9a7ba921e985f1a2b5387cb14167f
sjnhhhhhhhhh <sjn0412hhh@sina.com>
2024-08-02 17:02:49 +0800

    version 1.0

---
+++ build/src/CMakeFiles/Extract.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Extract.dir/extract_c++.cpp.obj"
+  "CMakeFiles/Extract.dir/extract_c++.cpp.obj.d"
+  "Extract.exe"
+  "Extract.exe.manifest"
+  "Extract.pdb"
+  "libExtract.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Extract.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
